30865340ba6cba418828155fc98db1247c890449
parsastar <parsaa921@gmail.com>
2025-05-26 16:36:10 +0330

    fixed position sticky bug and added back button for single blog page and its responsive

--- parsa-star-blog/src/app/blogs/[id]/page.tsx
+++ parsa-star-blog/src/app/blogs/[id]/page.tsx
@@ -1,8 +1,7 @@
 import { GetBlog } from "@/app/api/blog";
 import AnimatedLine from "@/components/animatedLine";
-import { Button } from "@/components/ui/button";
+import BackButton from "@/components/backButton";
 import { SingleBlogTransfer } from "@/utils/blog";
-import { ArrowLeft } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
@@ -12,29 +11,27 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
     const blog = SingleBlogTransfer({ blog: rawBlog });
 
     return (
-        <div className="container relative bg-red-400  mx-auto h-[100svh] flex flex-col items-center">
+        <div className="container relative   mx-auto min-h-[60svh] flex flex-col items-center">
             {/* Header */}
             <div className="w-full items-center flex justify-between">
-                <Button
-                    variant={"ghost"}
-                    className="flex items-center w-[calc(25%-1.5px)] rounded-none translate-x-[.5px] justify-center text-red-500 bg-secondary-500 duration-200 hover:text-secondary-500 p-10"
-                >
-                    <ArrowLeft className="!size-10" />
-                </Button>
+                <BackButton />
                 <p className="text-darkGrey-500 px-2 font-mono h-fit text-subtitle w-[calc(25%-1.5px)] translate-x-[.5px] text-right">
                     {blog.date}
                 </p>
             </div>
+            <div className="flex sm:hidden w-full">
+                <AnimatedLine />{" "}
+            </div>
 
             {/* Sticky Content + Image Section */}
-            <div className="w-full bg-blue-400 sticky top-0 flex">
-                <div className="flex w-full ">
+            <div className="w-full  sticky top-0 flex">
+                <div className="flex sm:flex-row flex-col w-full ">
                     {/* Left: Content section */}
-                    <div className="w-1/2 aspect-square flex flex-col translate-x-[0.5px]">
+                    <div className="w-full sm:w-1/2 aspect-square flex flex-col translate-x-[0.5px]">
                         <AnimatedLine damping={20} />
 
                         <div className="bg-secondary-500 px-2 py-5">
-                            <h1 className="text-3xl font-bold font-roboto_mono">
+                            <h1 className="text-blogTitle lg:text-3xl font-bold font-roboto_mono">
                                 {blog.title}
                             </h1>
                         </div>
@@ -42,7 +39,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
                         <AnimatedLine />
 
                         <div className="bg-secondary-500 px-2 py-5 flex-grow overflow-y-auto">
-                            <p className="text-description font-roboto text-darkGrey-500 leading-relaxed whitespace-pre-line">
+                            <p className="text-description lg:text-subtitle  font-roboto text-darkGrey-500 leading-relaxed whitespace-pre-line">
                                 {blog.body}
                             </p>
                         </div>
@@ -51,7 +48,8 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
                     </div>
 
                     {/* Right: Image section */}
-                    <div className="w-1/2 aspect-square shrink-0 left-0 bg-secondary-500 translate-x-[-1px] relative">
+
+                    <div className="w-full sm:order-[10] order-[-1]   sm:w-1/2 aspect-square shrink-0 left-0 bg-secondary-500 translate-x-[-1px] relative">
                         <Image
                             src={blog.image}
                             alt={blog.title}
