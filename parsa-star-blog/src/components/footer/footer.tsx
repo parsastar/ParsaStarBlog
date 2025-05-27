b5e752abf525d9a9bc95e56c8de5567e2c511f85
parsastar <parsaa921@gmail.com>
2025-05-27 20:46:09 +0330

    fixed build big

--- parsa-star-blog/src/components/footer/footer.tsx
+++ parsa-star-blog/src/components/footer/footer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, {  useRef } from "react";
 import FooterContent from "./footerCotent";
 import FooterTitle from "./footerTitle";
 import { useScroll } from "motion/react";
