6c7eacba7285d13cdff67099706b1364ec931912
parsastar <parsaa921@gmail.com>
2025-07-10 11:50:23 +0330

    fixed some schema indexing for more query speed (blogs and blog Comments )

--- parsa-star-blog/src/db/schema/blogs/blog.ts
+++ parsa-star-blog/src/db/schema/blogs/blog.ts
@@ -7,18 +7,25 @@ import { userT } from "../user";
 
 type TSubSection = { title: string; description: string };
 
-export const blogT = t.pgTable("blogs", {
-    id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
-    title: t.varchar({ length: 150 }).notNull(),
-    cover: t.varchar({ length: 500 }),
-    description: t.text().notNull(),
-    user_id: t
-        .integer()
-        .notNull()
-        .references(() => userT.id, { onDelete: "cascade" }),
-    sub_sections: t.json().$type<TSubSection[]>(),
-    ...defaultTimeStamps,
-});
+export const blogT = t.pgTable(
+    "blogs",
+    {
+        id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
+        title: t.varchar({ length: 150 }).notNull(),
+        cover: t.varchar({ length: 500 }),
+        description: t.text().notNull(),
+        user_id: t
+            .integer()
+            .notNull()
+            .references(() => userT.id, { onDelete: "cascade" }),
+        sub_sections: t.json().$type<TSubSection[]>(),
+        ...defaultTimeStamps,
+    },
+    (table) => [
+        t.index("user_id_index").on(table.user_id),
+        t.index("title_index").on(table.title),
+    ]
+);
 
 /// Relations for blogs
 export const blogsTableRelations = relations(blogT, ({ many, one }) => ({
--- parsa-star-blog/src/db/schema/blogs/blogComments.ts
+++ parsa-star-blog/src/db/schema/blogs/blogComments.ts
@@ -4,22 +4,32 @@ import * as t from "drizzle-orm/pg-core";
 import { userT } from "../user";
 import { blogT } from "./blog";
 
-export const BlogCommentsT = t.pgTable("blog_comments", {
-    id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
-    content: t.text().notNull(),
-    user_id: t
-        .integer()
-        .notNull()
-        .references(() => userT.id, { onDelete: "cascade" }),
-    blog_id: t
-        .integer()
-        .notNull()
-        .references(() => blogT.id, { onDelete: "cascade" }),
-    reply_to_id: t.integer().references((): t.AnyPgColumn => BlogCommentsT.id, {
-        onDelete: "cascade",
-    }), // self-referencing FK
-    ...defaultTimeStamps,
-});
+export const BlogCommentsT = t.pgTable(
+    "blog_comments",
+    {
+        id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
+        content: t.text().notNull(),
+        user_id: t
+            .integer()
+            .notNull()
+            .references(() => userT.id, { onDelete: "cascade" }),
+        blog_id: t
+            .integer()
+            .notNull()
+            .references(() => blogT.id, { onDelete: "cascade" }),
+        reply_to_id: t
+            .integer()
+            .references((): t.AnyPgColumn => BlogCommentsT.id, {
+                onDelete: "cascade",
+            }), // self-referencing FK
+        ...defaultTimeStamps,
+    },
+    (table) => [
+        t.index("user_id_index").on(table.user_id),
+        t.index("blog_id_index").on(table.blog_id),
+        t.index("reply_to_id_index").on(table.reply_to_id),
+    ]
+);
 
 export const BlogCommentsTableRelations = relations(
     BlogCommentsT,
